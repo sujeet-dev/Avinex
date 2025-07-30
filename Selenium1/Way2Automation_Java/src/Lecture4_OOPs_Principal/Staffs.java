ffa500e8d83878bc8aed5770aa8ee6788a214d2e
Sujeet Singh <sujeet@avinex9.com>
2025-07-30 11:30:13 +0530

    Datadriven

--- Selenium1/Way2Automation_Java/src/Lecture4_OOPs_Principal/Staffs.java
+++
@@ -1,9 +0,0 @@
-package Lecture4_OOPs_Principal;
-
-public class Staffs {
-
-	
-	public void maintanense() {
-		System.out.println("Maintains all the staff");
-	}
-}
--- Selenium1/Way2Automation_Java/src/Lecture5_Abstracrion/RBI.java
+++
@@ -1,9 +0,0 @@
-package Lecture5_Abstracrion;
-
-public interface RBI 
-{
-	public void deposit();
-	public void withdraw();
-	public void giveInterest();
-
-}
